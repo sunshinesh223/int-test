5bb926f256b0125ffb95d3b6f5660602c583f255
Guang Yang <guangyang@ticktack.no>
2021-11-02 20:18:30 +0100

    update

--- get_image.sh
+++ get_image.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 
-img_id=$(packer build builder.json|sed -nr "s/.*ID: ([0-9]+).*/\1/p")
-echo "get img_id as $img_id"
+packer build builder.json> build.log
+img_id=$(cat build.log|sed -nr "s/.*ID: ([0-9]+).*/\1/p")
+echo "$img_id"
