4b0cbd5f32cc8f99945ae5a0de7f93eb8c9d3673
Guang Yang <guangyang@ticktack.no>
2021-11-03 10:07:55 +0100

    add deploy.sh and inventory for ansible

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,15 @@ ssh_pub_id=$(doctl compute ssh-key list|grep $KeyName|awk '{ print $1 }')
 img_id=$(cat img_id.txt)
 
 #create droplet
-doctl compute droplet create --image $img_id --size s-1vcpu-1gb --ssh-keys $ssh_pub_id --region ams3 test.com
+dropet_info=$(doctl compute droplet create --image $img_id --size s-1vcpu-1gb --ssh-keys $ssh_pub_id --region ams3 $RANDOM.int-test.com|grep -v ID|awk '{ print $1,$2 }')
+dropet_id=$(echo $dropet_info|awk '{ print $1}')
+dropet_name=$(echo $dropet_info|awk '{ print $2}')
+while true; then
+	dropet_ip=$(doctl compute droplet get $dropet_id|grep -v ID|awk '{ print $3 }'|grep .)
+	if [ $? eq 0 ]; then
+		break
+	fi
+done
+# save the new droplet for ansible inventory
+echo "$dropet_name=$dropet_ip" >>inventory
+
